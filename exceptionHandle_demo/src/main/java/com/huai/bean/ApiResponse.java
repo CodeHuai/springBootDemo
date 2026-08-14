64d3dbffc457cc6ba99e9d1c39458e796b950706
MrHuai <andbreak@outlook.com>
2026-08-14 16:05:20 +0800

    返回体信封

---
+++ exceptionHandle_demo/src/main/java/com/huai/bean/ApiResponse.java
@@ -0,0 +1,104 @@
+package com.huai.bean;
+
+import com.huai.constant.Status;
+import com.huai.exception.BaseException;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse {
+    /**
+     * 状态码
+     */
+    private Integer code;
+
+    /**
+     * 返回内容
+     */
+    private String message;
+
+    /**
+     * 返回数据
+     */
+    private Object data;
+
+    /**
+     * 自定义的消息体
+     *
+     * @param code    状态码
+     * @param message 信息
+     * @param data    数据体
+     * @return ApiResponse
+     */
+    public static ApiResponse of(Integer code, String message, Object data) {
+        return new ApiResponse(code, message, data);
+    }
+
+    /**
+     * 构造一个成功且带数据的API返回
+     *
+     * @param data 返回数据
+     * @return ApiResponse
+     */
+    public static ApiResponse ofSuccess(Object data) {
+        return ofStatus(Status.OK, data);
+    }
+
+    /**
+     * 构造一个成功且自定义消息的API返回
+     *
+     * @param message 返回内容
+     * @return ApiResponse
+     */
+    public static ApiResponse ofMessage(String message) {
+        return of(Status.OK.getCode(), message, null);
+    }
+
+    /**
+     * 构造一个只有状态的消息体
+     *
+     * @param status 状态
+     * @return ApiResponse
+     */
+    public static ApiResponse ofStatus(Status status) {
+        return ofStatus(status, null);
+    }
+
+    /**
+     * 构造一个有状态且带有data的返回体
+     *
+     * @param status 状态
+     * @param data   数据
+     * @return ApiResponse
+     */
+    public static ApiResponse ofStatus(Status status, Object data) {
+        return of(status.getCode(), status.getMessage(), data);
+    }
+
+
+    /**
+     * 构造一个异常且带数据的API返回
+     *
+     * @param t    异常
+     * @param data 返回数据
+     * @param <T>  {@link BaseException} 的子类
+     * @return ApiResponse
+     */
+    public static <T extends BaseException> ApiResponse ofException(T t, Object data) {
+        return of(t.getCode(), t.getMessage(), data);
+    }
+
+    /**
+     * 构造一个异常且不带数据的API返回
+     *
+     * @param t   异常
+     * @param <T> {@link BaseException} 的子类
+     * @return ApiResponse
+     */
+    public static <T extends BaseException> ApiResponse ofException(T t) {
+        return of(t.getCode(), t.getMessage(), null);
+    }
+}
